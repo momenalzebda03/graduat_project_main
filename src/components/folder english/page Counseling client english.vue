cae612d1ddbb6ac16abb793a81827beee4bc6d8e
unknown <momen07996@gmail.com>
2023-08-01 15:26:26 +0300

    update the back end in the pages counseling client and vocaional

--- src/components/folder english/page Counseling client english.vue
+++ src/components/folder english/page Counseling client english.vue
@@ -3,18 +3,19 @@
     <section class="container pt-4 my-5">
         <div class="text-center">
             <h2 class="fw-bold" :style="{ color: textColor }">Send a message to the vocational you want<br><span
-                    class="p_color_blue">Name client</span>
+                    class="p_color_blue">{{ api_user1.Customer_Name }}</span>
             </h2>
         </div>
         <div class="row">
             <div class="col-12 col-lg-4">
                 <div class="d-flex justify-content-center position-relative">
-                    <img src="../../assets/imagedatabase/ai-image-enlarger-1-before-2.jpg"
+                    <img :src="getImagePath(api_user1.Customer_Image)"
                         class="bg-white rounded-circle image_point border border-2 p-3" alt="">
                     <button @click="button_block"
                         class="position-absolute top-50 start-50 border border-2 text-white py-1 px-3 rounded-4 centered_button">Send
                         A Message</button>
-                    <textarea class="position-absolute text_top rounded-3 ps-1 pt-1" placeholder="send message"></textarea>
+                    <textarea required v-model="text_message" class="position-absolute text_top rounded-3 ps-1 pt-1"
+                        placeholder="send message"></textarea>
                 </div>
             </div>
             <div class="col-12 col-lg-8 mt-5">
@@ -24,125 +25,48 @@
                     <span class="fs-4 fw-bold">Send</span>
                 </div>
                 <div class="overflow-y-auto overflow-x-hidden mt-4 div_overflow h-50">
-                    <div class="d-flex justify-content-between align-items-center flex-column flex-md-row">
+                    <div class="d-flex justify-content-between align-items-center flex-column flex-md-row py-3"
+                        v-for="my_api in api_user" :key="my_api.Customer_Id">
+                        <p class="d-none">{{ message_vocaional(my_api.id) }}</p>
                         <div class="d-flex gap-0 gap-md-4 align-items-center flex-column flex-md-row">
                             <img src="../../assets/imagedatabase/ai-image-enlarger-1-before-2.jpg" alt=""
                                 class="image_table rounded-circle">
-                            <span :style="{ color: textColor }">Name vocational</span>
-                            <div class="d-flex flex-column">
-                                <input type="text">
+                            <span :style="{ color: textColor }">{{ my_api.Name_Vocational }}</span>
+                            <div v-for="select_message in getMessagesByApiUserId(my_api.id)" :key="select_message.id"
+                                class="d-flex flex-column">
+                                <input type="text" :value="select_message.message" readonly>
                             </div>
                         </div>
-                        <a href="#">
+                        <a :href="getLink(my_api.id)">
                             <button type="submit" :style="{ backgroundColor: buttonColor }"
                                 class="button_color text-white border-0 btn rounded-5 px-5 bg-success mt-3">Reply</button>
                         </a>
                     </div>
-                    <div class="div_hr_button my-2"></div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- end main -->
+    <!-- end main
+    start completed 1 -->
+    <section :style="{ display: divWhiteDisplay1 }">
+        <div class="d-flex justify-content-center">
+            <div class="div_white shadow-lg p-3 mb-5 bg-body position-fixed">
+                <div class="text-end pe-3">
+                    <div id="icon_close1">
+                        <i class="fas fa-times fs-2" @click="hideDivWhite1"></i>
+                    </div>
+                    <div class="text-center container">
+                        <h4 class="text-success">Message has been sent</h4>
+                        <img src="../../assets/verificationimage/imagetrue.png" class="py-5 w-75" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end completed 1 -->
 </template>
 
 <script>
-export default {
-    name: "ComponentHome",
-    computed: {
-        textColor() {
-            return this.$root.textColor;
-        },
-    },
-    created() {
-        this.changePageTitle('COUNSELING CLIENT');
-    },
-    methods: {
-        changePageTitle(newTitle) {
-            document.title = newTitle;
-        },
-        button_block() {
-            const text_top = document.querySelector(".text_top");
-            text_top.style.width = "60%";
-            text_top.style.height = "60%";
-            text_top.style.zIndex = "1";
-        }
-    }
-}
-</script>
-<!-- <template>
-    <div>
-        start main
-        <section class="mt-4 text_black" :style="{ color: textColor }">
-            <div class="text-center container">
-                <div class="d-flex flex-column flex-lg-row my-5">
-                    <div class="w-100 me-0 me-lg-1">
-                        <h4>We Help And Solve Your Business Problem</h4>
-                        <h5>Customers</h5>
-                        <p class="mt-4">{{ api_user1.Customer_Name }}</p>
-                        <div>
-                            <img :src="getImagePath(api_user1.Customer_Image)" class="image_header" alt="">
-                            <div class="d-flex justify-content-center">
-                                <div class="div_before_image"></div>
-                            </div>
-                            <br>
-                            <textarea v-model="text_message" class="mt-3 pt-2 ps-2 rounded-3 my_texteara"
-                                placeholder="Sent Message..." cols="60" rows="7"></textarea>
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <div :style="{ backgroundColor: buttonColor }"
-                            class="position-absolute div_wh div_wh_english d-flex justify-content-center align-items-center">
-                        </div>
-                    </div>
-                    <div class="w-100 mt-5 pt-5 ms-0 ms-lg-2 pt-lg-0 mt-lg-0">
-                        <h4>We Help And Solve Your Business Problem</h4>
-                        <h5>Vocational</h5>
-                        <div v-for="my_api in api_user" :key="my_api.Customer_Id">
-                            <p class="d-none">{{ message_vocaional(my_api.id) }}</p>
-                            <p class="mt-4">{{ my_api.Name_Vocational }}</p>
-                            <img :src="getImagePath(my_api.Vocational_Image)" class="image_header" alt="">
-                            <div class="d-flex justify-content-center">
-                                <div class="div_before_image"></div>
-                            </div>
-                            <br>
-                            <div v-for="select_message in getMessagesByApiUserId(my_api.id)" :key="select_message.id">
-                                <input type="text" :value="select_message.message" readonly>
-                                <br>
-                                <br>
-                            </div>
-                            <br>
-                            <a :href="getLink(my_api.id)">
-                                <button type="submit" :style="{ backgroundColor: buttonColor }"
-                                    class="button_color text-white border-0 btn rounded-5 px-5 bg-success mt-3">Reply</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        end main
-        start completed 1
-        <section :style="{ display: divWhiteDisplay1 }">
-            <div class="d-flex justify-content-center">
-                <div class="div_white shadow-lg p-3 mb-5 bg-body position-fixed">
-                    <div class="text-end pe-3">
-                        <div id="icon_close1">
-                            <i class="fas fa-times fs-2" @click="hideDivWhite1"></i>
-                        </div>
-                        <div class="text-center container">
-                            <h4 class="text-success">Message has been sent</h4>
-                            <img src="../../assets/verificationimage/imagetrue.png" class="py-5 w-75" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        end completed 1
-    </div>
-</template> -->
-
-<!-- <script>
 import "../../App.vue";
 import axios from 'axios';
 
@@ -188,6 +112,12 @@ export default {
             });
     },
     methods: {
+        button_block() {
+            var text_top = document.querySelector(".text_top");
+            text_top.style.width = "60%";
+            text_top.style.height = "60%";
+            text_top.style.zIndex = "1";
+        },
         hideDivWhite1() {
             this.divWhiteDisplay1 = 'none';
         },
@@ -217,4 +147,4 @@ export default {
         },
     },
 }
-</script> -->
+</script>
